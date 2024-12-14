b6d7db865088ae6251886c60067f32400b65ea0b
abracodeabra <akakiplimo@gmail.com>
2024-12-14 21:11:12 +0300

    design home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,18 @@
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, Image, FlatList} from 'react-native';
 import React from 'react';
 import ScreenWrapper from '../components/screenWrapper';
 import {colors} from '../theme';
+import randomImage from '../assets/images/randomImage';
+
+const items = [
+  {id: 1, place: 'Malindi', country: 'Kenya'},
+  {id: 2, place: 'Zanzibar', country: 'Tanzania'},
+  {id: 3, place: 'Kigali', country: 'Rwanda'},
+  {id: 4, place: 'Juba', country: 'South Sudan'},
+  {id: 5, place: 'Mogadishu', country: 'Somalia'},
+  {id: 6, place: 'Bujumbura', country: 'Burundi'},
+  {id: 7, place: 'Kampala', country: 'Uganda'},
+];
 
 export default function HomeScreen() {
   return (
@@ -13,6 +24,52 @@ export default function HomeScreen() {
         <TouchableOpacity className="p-2 px-3 bg-white border border-gray-200 rounded-full">
           <Text className={colors.heading}>Logout</Text>
         </TouchableOpacity>
+      </View>
+
+      <View className="flex-row justify-center items-center bg-yellow-100 rounded-xl mx-4 mb-4">
+        <Image
+          source={require('../assets/images/banner.png')}
+          className="w-80 h-60"
+        />
+      </View>
+
+      <View className="px-4 space-y-3">
+        <View className="flex-row justify-between items-center">
+          <Text className={`${colors.heading} font-bold text-xl`}>
+            Recent Trips
+          </Text>
+          <TouchableOpacity className="p-2 px-3 bg-white border border-gray-200 rounded-full">
+            <Text className={colors.heading}>Add Trip</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View className="mt-4" style={{height: 450}}>
+          <FlatList
+            data={items}
+            keyExtractor={item => item.id}
+            numColumns={2}
+            showsVerticalScrollIndicator={false}
+            columnWrapperStyle={{
+              justifyContent: 'space-between',
+            }}
+            className="mx-3"
+            renderItem={({item}) => {
+              return (
+                <TouchableOpacity className="bg-white p-5 mx-1 rounded-2xl mb-3 shadow-sm">
+                  <View>
+                    <Image source={randomImage()} className="w-36 h-36 mb-2" />
+                    <Text className={`${colors.heading} font-bold`}>
+                      {item.place}
+                    </Text>
+                    <Text className={`${colors.heading} text-xs`}>
+                      {item.country}
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              );
+            }}
+          />
+        </View>
       </View>
     </ScreenWrapper>
   );
