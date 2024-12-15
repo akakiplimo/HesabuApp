94535094b8cf03dbd870021f2391c46364e09d9e
abracodeabra <akakiplimo@gmail.com>
2024-12-15 03:49:32 +0300

    add redux toolkit for user auth management

--- navigation/appNavigation.js
+++ navigation/appNavigation.js
@@ -8,51 +8,63 @@ import TripExpenses from '../screens/TripExpenses';
 import WelcomeScreen from '../screens/WelcomeScreen';
 import SignUpScreen from '../screens/SignUpScreen';
 import SignInScreen from '../screens/SignInScreen';
+import {useSelector} from 'react-redux';
 
 const Stack = createNativeStackNavigator();
 
 function AppNavigation() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome">
-        <Stack.Screen
-          options={{headerShown: false, presentation: 'modal'}}
-          name="SignUp"
-          component={SignUpScreen}
-        />
-        <Stack.Screen
-          options={{headerShown: false, presentation: 'modal'}}
-          name="SignIn"
-          component={SignInScreen}
-        />
-        <Stack.Screen
-          options={{headerShown: false}}
-          name="Welcome"
-          component={WelcomeScreen}
-        />
-        <Stack.Screen
-          options={{headerShown: false}}
-          name="Home"
-          component={HomeScreen}
-        />
-        <Stack.Screen
-          options={{headerShown: false}}
-          name="AddTrip"
-          component={AddTrip}
-        />
-        <Stack.Screen
-          options={{headerShown: false}}
-          name="AddExpense"
-          component={AddExpense}
-        />
-        <Stack.Screen
-          options={{headerShown: false}}
-          name="TripExpenses"
-          component={TripExpenses}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  const {user} = useSelector(state => state.user);
+
+  if (user) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            options={{headerShown: false}}
+            name="Home"
+            component={HomeScreen}
+          />
+          <Stack.Screen
+            options={{headerShown: false}}
+            name="AddTrip"
+            component={AddTrip}
+          />
+          <Stack.Screen
+            options={{headerShown: false}}
+            name="AddExpense"
+            component={AddExpense}
+          />
+          <Stack.Screen
+            options={{headerShown: false}}
+            name="TripExpenses"
+            component={TripExpenses}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  } else {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Welcome">
+          <Stack.Screen
+            options={{headerShown: false, presentation: 'modal'}}
+            name="SignUp"
+            component={SignUpScreen}
+          />
+          <Stack.Screen
+            options={{headerShown: false, presentation: 'modal'}}
+            name="SignIn"
+            component={SignInScreen}
+          />
+          <Stack.Screen
+            options={{headerShown: false}}
+            name="Welcome"
+            component={WelcomeScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 export default AppNavigation;
